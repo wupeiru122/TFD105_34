ea81ea611662960825cc893ef9ac9c20059ac394
wupeiru122 <qwejkk12372@gmail.com>
2022-02-25 22:11:05 +0800

    0225

--- js/slick.js
+++ js/slick.js
@@ -11,7 +11,7 @@ $(document).on('ready', function() {
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 2,
         infinite: true,
         dots: true
@@ -34,7 +34,7 @@ $(document).on('ready', function() {
     {
       breakpoint: 530,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1
       }
     }
